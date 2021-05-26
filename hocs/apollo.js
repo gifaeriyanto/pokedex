75e76171b57b0c385dcf967f364e85ffb2bfb5dd
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2021-05-27 04:42:30 +0800

    fix(graphql): update graphql url

--- hocs/apollo.js
+++ hocs/apollo.js
@@ -1,9 +1,9 @@
-import { withData } from 'next-apollo';
-import { HttpLink } from 'apollo-boost';
+import { withData } from "next-apollo";
+import { HttpLink } from "apollo-boost";
 
 const config = {
   link: new HttpLink({
-    uri: 'https://graphql-pokemon.now.sh/',
+    uri: "https://graphql-pokemon2.vercel.app/",
   }),
 };
 
